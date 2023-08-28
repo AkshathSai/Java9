ebb9d7e58fe195d1330b5325002fe2c9fefef1fe
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2023-08-28 13:39:21 +0530

    Project refactoring

---
+++ modularized-service-jar/src/main/java/org/example/sorting/util/SortingUtils.java
@@ -0,0 +1,15 @@
+package org.example.sorting.util;
+
+import org.example.sorting.algorithm.BubbleSort;
+
+import java.util.List;
+
+public class SortingUtils {
+
+    public List<String> sort(List<String> names) {
+
+        BubbleSort bubbleSort = new BubbleSort();
+        return bubbleSort.sort(names);
+    }
+
+}
